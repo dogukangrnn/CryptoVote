2f49cc896328ac05fabd869029bdc37c5fd9c178
Dogukan Goren <dogukangrnn@gmail.com>
2026-04-18 09:59:36 -0400

    Store encrypted votes in ballot box

--- voting.py
+++ voting.py
@@ -1,45 +1,44 @@
 import json
-import secrets
 from database import VOTERS_FILE, BALLOT_FILE, read_voters, read_ballots
+from crypto_utils import encrypt_vote, generate_keys
+import secrets
+
 
 def save_voters(voters):
     with open(VOTERS_FILE, "w", encoding="utf-8") as f:
         json.dump(voters, f, ensure_ascii=False, indent=4)
 
+
 def save_ballots(ballots):
     with open(BALLOT_FILE, "w", encoding="utf-8") as f:
         json.dump(ballots, f, ensure_ascii=False, indent=4)
 
+
 def generate_token():
     return secrets.token_hex(8)
 
-def find_voter(tc):
-    voters = read_voters()
-    for voter in voters:
-        if voter["tc"] == tc:
-            return voter
-    return None
 
 def vote(tc, vote_choice):
+    generate_keys()
+
     voters = read_voters()
     ballots = read_ballots()
 
     for voter in voters:
         if voter["tc"] == tc:
-
             if voter["oy_kullandi"]:
                 print("❌ Bu kişi zaten oy kullanmış!")
                 return
 
-            # token üret
             token = generate_token()
+            encrypted_vote = encrypt_vote(vote_choice)
+
             voter["token"] = token
             voter["oy_kullandi"] = True
 
-            # oy kaydı (şimdilik şifreleme yok)
             ballot = {
                 "token": token,
-                "vote": vote_choice
+                "encrypted_vote": encrypted_vote
             }
 
             ballots.append(ballot)
@@ -53,3 +52,8 @@ def vote(tc, vote_choice):
 
     print("❌ Seçmen bulunamadı!")
 
+
+if __name__ == "__main__":
+    tc = input("TC Kimlik No: ")
+    vote_choice = input("Oy tercihi: ")
+    vote(tc, vote_choice)
